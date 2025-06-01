f32dbe8f24d700fdc64fc1b923fa50146a8ba338
Thorrak <thorrak@excelinspace.com>
2025-06-01 01:39:55 -0400

    Add chart placeholder

--- src/components/calibration/CalibrationPanel.vue
+++ src/components/calibration/CalibrationPanel.vue
@@ -17,6 +17,12 @@
         <div class="w-full flex flex-col">
           <div class="w-full max-w-2xl aspect-square">
             <Line v-if="combinedChartData" :data="combinedChartData" :options="chartOptions" />
+            <div v-else class="w-full h-full bg-gray-100 border-2 border-dashed border-gray-300 rounded-lg flex flex-col items-center justify-center text-gray-500">
+              <svg class="w-16 h-16 mb-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path>
+              </svg>
+              <p class="text-center text-sm">{{ $t('calibration.chart_placeholder_message') }}</p>
+            </div>
           </div>
         </div>
       </div>
